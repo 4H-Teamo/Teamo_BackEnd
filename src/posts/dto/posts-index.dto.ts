393ecce6a442f9e2534117fa7dff9ec2185e15c7
scorchedrice <wldnd2977@gmail.com>
2025-07-29 02:38:35 +0900

    ♻️ refactor: validation.message.ts 위치 shared로 변경

--- src/posts/dto/posts-index.dto.ts
+++ src/posts/dto/posts-index.dto.ts
@@ -1,16 +1,28 @@
 import { IsOptional, IsPositive, IsInt } from 'class-validator';
 import { Type } from 'class-transformer';
+import {
+  IntValidationMessage,
+  PositiveIntValidationMessage,
+} from '../../shared/validators/messages/validation.message';
 
 export class PostsIndexDto {
   @IsOptional()
   @Type(() => Number) // 문자열 → 숫자 변환
-  @IsInt()
-  @IsPositive()
+  @IsInt({
+    message: IntValidationMessage,
+  })
+  @IsPositive({
+    message: PositiveIntValidationMessage,
+  })
   page?: number = 1;
 
   @IsOptional()
   @Type(() => Number)
-  @IsInt()
-  @IsPositive()
+  @IsInt({
+    message: IntValidationMessage,
+  })
+  @IsPositive({
+    message: PositiveIntValidationMessage,
+  })
   limit?: number = 10;
 }
