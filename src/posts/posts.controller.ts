ed1b87cff3f3a12bdd13c9ae88a264ec6c4d0f4d
scorchedrice <wldnd2977@gmail.com>
2025-07-29 03:29:56 +0900

    ✨ feat: posts(index, create)

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { PostsIndexDto } from './dto/posts-index.dto';
+import { PostsCreateDto } from './dto/posts-create.dto';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  index(@Query('limit') limit: string, @Query('page') page: string) {
-    return this.postsService.index(limit, page);
+  index(@Query() query: PostsIndexDto) {
+    return this.postsService.index(query);
+  }
+
+  @Post()
+  create(@Body() body: PostsCreateDto) {
+    const userId = 'cc7a79ce-3b56-4022-b098-09be5d2f482b'; //TODO : 나중에 guard로 유저아이디를 받는다.
+    console.log(body);
+    return this.postsService.create(body, userId);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { PostsIndexDto } from './dto/posts-index.dto';
+import { PostsCreateDto } from './dto/posts-create.dto';
+import { handlePrismaError } from '../shared/validators/prisma/prisma.exception';
 
 @Injectable()
 export class PostsService {
   constructor(private prisma: PrismaService) {}
 
-  async index(page: string, limit: string) {
-    const pageNumber = parseInt(page, 10) || 1;
-    const limitNumber = parseInt(limit, 10) || 10;
+  async index(query: PostsIndexDto) {
+    const pageNumber = query.page || 1;
+    const limitNumber = query.limit || 10;
 
     const skip = (pageNumber - 1) * limitNumber;
 
@@ -19,5 +22,25 @@ export class PostsService {
       },
     });
     return posts;
+  }
+
+  async create(body: PostsCreateDto, userId: string) {
+    try {
+      const post = await this.prisma.post.create({
+        data: {
+          title: body.title,
+          content: body.content,
+          workMode: body.workMode,
+          endDate: body.endDate,
+          capacity: body.capacity,
+          stacks: body.stacks,
+          positions: body.positions,
+          userId,
+        },
+      });
+      return post;
+    } catch (error) {
+      handlePrismaError(error);
+    }
   }
 }
