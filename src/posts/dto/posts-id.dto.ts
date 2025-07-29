0539c38fc00d20bf58e95a5c6cc41ab5e7423efe
scorchedrice <wldnd2977@gmail.com>
2025-07-30 03:08:53 +0900

    💡update: post-id.dto validation message

--- src/posts/dto/posts-id.dto.ts
+++ src/posts/dto/posts-id.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsUUID } from 'class-validator';
+import { UuidValidationMessage } from '../../shared/validators/messages/validation.messages';
 
 export class PostsIdDto {
   @ApiProperty({
     example: 'ABCDE-... (게시글 uuid)',
     description: '게시글 아이디',
   })
-  @IsUUID()
+  @IsUUID(4, { message: UuidValidationMessage })
   postId: string;
 }
