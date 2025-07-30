043964ff95cd1455a59760a0e7897fa6997ce6dd
scorchedrice <wldnd2977@gmail.com>
2025-07-30 20:48:41 +0900

    🐛 fix: dto명칭 수정 반영

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body, Param, Patch, Delete } from '@nestjs/common';
 import { CommentsService } from './comments.service';
-import { PostsIdDto } from '../posts/dto/posts-id.dto';
+import { PostIdDto } from '../posts/dto/post-id.dto';
 import { CommentsCreateDto } from './dto/comments-create.dto';
 import { CommentsIdDto } from './dto/comments-id.dto';
 
@@ -9,7 +9,7 @@ export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @Post('posts/:postId/comments')
-  create(@Param() param: PostsIdDto, @Body() body: CommentsCreateDto) {
+  create(@Param() param: PostIdDto, @Body() body: CommentsCreateDto) {
     const userId = 'cc7a79ce-3b56-4022-b098-09be5d2f482b'; //TODO : 나중에 guard로 유저아이디를 받는다.
     return this.commentsService.create(param.postId, userId, body);
   }
