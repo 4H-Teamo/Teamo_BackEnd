ddf76728a7374475ac811cf147de27ecd17b2166
HS <mcm009@naver.com>
2025-08-08 20:20:46 +0900

    💡 update: user API JWT AuthGuard 반영

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,8 +11,8 @@ import {
 import { UsersService } from './users.service';
 import { UsersIndexDto } from './dto/users-index.dto';
 import { UserUpdateDto } from './dto/users-update.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { RequestWithUser } from 'src/shared/interfaces/auth.interface';
+import { JwtAuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -23,19 +23,19 @@ export class UsersController {
     return this.usersService.index(query);
   }
 
-  @UseGuards(AuthGuard('access'))
+  @UseGuards(JwtAuthGuard)
   @Get('me')
   show(@Req() req: RequestWithUser) {
     return this.usersService.show(req.user.userId);
   }
 
-  @UseGuards(AuthGuard('access'))
+  @UseGuards(JwtAuthGuard)
   @Patch()
   update(@Body() body: UserUpdateDto, @Req() req: RequestWithUser) {
     return this.usersService.update(body, req.user.userId);
   }
 
-  @UseGuards(AuthGuard('access'))
+  @UseGuards(JwtAuthGuard)
   @Delete()
   destroy(@Req() req: RequestWithUser) {
     return this.usersService.destroy(req.user.userId);
