569f4e68727a3a9381eb7c31ae88233034c51d35
HS <mcm009@naver.com>
2025-08-08 20:22:36 +0900

    💡 update: notice API JWT AuthGuard 반영

--- src/notices/notices.module.ts
+++ src/notices/notices.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NoticesService } from './notices.service';
 import { NoticesController } from './notices.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [NoticesController],
   providers: [NoticesService],
 })
