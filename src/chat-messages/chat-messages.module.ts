41de28d5576e3c041040c7e2aefeece04075a5f8
HS <mcm009@naver.com>
2025-08-30 10:42:20 +0900

    🐛 fix: jwtGuard 사용을 위해 module import

--- src/chat-messages/chat-messages.module.ts
+++ src/chat-messages/chat-messages.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatMessagesService } from './chat-messages.service';
 import { ChatMessagesController } from './chat-messages.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ChatMessagesController],
   providers: [ChatMessagesService],
 })
--- src/chat-rooms/chat-rooms.module.ts
+++ src/chat-rooms/chat-rooms.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatRoomsService } from './chat-rooms.service';
 import { ChatRoomsController } from './chat-rooms.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ChatRoomsController],
   providers: [ChatRoomsService],
 })
